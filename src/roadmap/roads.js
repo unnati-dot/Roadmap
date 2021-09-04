d5708f57917fe9d167aefc50f7f2f91bf587010a
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-09-04 22:56:18 +0530

    update

---
+++ src/roadmap/roads.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: "Frontend",
+    },
+    {
+        id: 2,
+        name: "Backend",
+    },
+    {
+        id: 3,
+        name: "React",
+    },
+    {
+        id: 4,
+        name: "Devops",
+    },
+    {
+        id: 5,
+        name: "Cyber Security",
+    },
+    {
+        id: 6,
+        name: "Android",
+    }
+]
